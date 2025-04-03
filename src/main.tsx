76be33b99d4aabb847d81ab0a31ca8da14a33fd2
shippo <297029445@qq.com>
2025-04-03 23:03:12 +0800

    Add static meta tags and dynamic meta tag updater

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
-import { StrictMode } from 'react';
+import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, useLocation } from 'react-router-dom';
 import Timer5 from './pages/Timer5';
 import Timer10 from './pages/Timer10';
 import Timer15 from './pages/Timer15';
@@ -8,30 +8,79 @@ import Timer20 from './pages/Timer20';
 import Timer30 from './pages/Timer30';
 import './index.css';
 
+// Meta tag updater component
+function MetaUpdater() {
+  const location = useLocation();
+
+  React.useEffect(() => {
+    // Find the meta tag for the current route
+    const metaTag = document.querySelector(`meta[data-route="${location.pathname}"]`);
+    if (metaTag) {
+      // Update the default meta description
+      const defaultMeta = document.querySelector('meta[name="description"]:not([data-route])');
+      if (defaultMeta) {
+        defaultMeta.setAttribute('content', metaTag.getAttribute('content') || '');
+      }
+    }
+  }, [location]);
+
+  return null;
+}
+
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Timer15 />,
+    element: (
+      <>
+        <MetaUpdater />
+        <Timer15 />
+      </>
+    ),
   },
   {
     path: "/5-minute-timer",
-    element: <Timer5 />,
+    element: (
+      <>
+        <MetaUpdater />
+        <Timer5 />
+      </>
+    ),
   },
   {
     path: "/10-minute-timer",
-    element: <Timer10 />,
+    element: (
+      <>
+        <MetaUpdater />
+        <Timer10 />
+      </>
+    ),
   },
   {
     path: "/15-minute-timer",
-    element: <Timer15 />,
+    element: (
+      <>
+        <MetaUpdater />
+        <Timer15 />
+      </>
+    ),
   },
   {
     path: "/20-minute-timer",
-    element: <Timer20 />,
+    element: (
+      <>
+        <MetaUpdater />
+        <Timer20 />
+      </>
+    ),
   },
   {
     path: "/30-minute-timer",
-    element: <Timer30 />,
+    element: (
+      <>
+        <MetaUpdater />
+        <Timer30 />
+      </>
+    ),
   },
 ]);
 
